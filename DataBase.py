510c2c253eead6adce0fa31b4dc7326aec6dab3a
add <1395918522@qq.com>
2023-06-29 18:49:54 +0800

    update:增加了注释信息 feat:增加了修改信息时的邮箱验证

--- DataBase.py
+++ DataBase.py
@@ -3,10 +3,16 @@ import re
 
 class MysqlDatabases:
     def __init__(self):
+        # 初始化数据库对象
+        # 从文件中加载用户数据和学生数据
         self.users = json.loads(open('users.json', mode='r', encoding='utf-8').read())
         self.students = json.loads(open('students.json', mode='r', encoding='utf-8').read())
 
     def check_login(self, username, password):
+        # 检查用户登录
+        # 根据提供的用户名和密码进行验证
+        # 如果用户名和密码匹配，则返回登录成功
+        # 否则返回登录失败的相应消息
         for user in self.users:
             if user['username'] == username:
                 if password == user['password']:
@@ -16,9 +22,13 @@ class MysqlDatabases:
         return False, '登陆失败，用户名错误'
 
     def all_student(self):
+        # 获取所有学生信息
+        # 返回存储的学生数据
         return self.students
 
     def is_valid_email(self, email):
+        # 验证邮箱格式是否正确
+        # 使用正则表达式检查提供的邮箱是否符合指定的格式要求
         regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
         if re.fullmatch(regex, email):
             # print('='*10, "ok", '='*10)
@@ -28,7 +38,10 @@ class MysqlDatabases:
             return False
 
     def insert(self, student:dict):
-
+        # 插入学生信息
+        # 首先验证学生的邮箱格式是否正确
+        # 如果邮箱格式正确，将学生信息添加到学生数据中，并将学生数据写入到文件中，然后返回插入成功的消息
+        # 如果邮箱格式不正确，返回插入失败的消息
         if not self.is_valid_email(student['email']):
             return False, '邮箱格式不正确'
         else:
@@ -38,6 +51,10 @@ class MysqlDatabases:
             return True, '插入成功'
 
     def delete_by_name(self, name):
+        # 根据姓名删除学生信息
+        # 遍历学生数据，找到姓名匹配的学生并删除
+        # 如果成功删除，将更新后的学生数据写入文件，并返回删除成功的消息
+        # 如果找不到匹配的学生，返回学生不存在的消息
         for stu in self.students:
             print(stu)
             if stu['name'] == name:
@@ -48,6 +65,10 @@ class MysqlDatabases:
         return False, f'{name} 不存在'
 
     def search_by_name(self, targetname):
+        # 根据姓名搜索学生信息
+        # 遍历学生数据，找到姓名匹配的学生并返回学生信息
+        # 如果找到匹配的学生，返回成功和学生信息
+        # 如果找不到匹配的学生，返回学生不存在的消息
         for stu in self.students:
             print(stu)
             if stu['name'] == targetname:
@@ -55,21 +76,35 @@ class MysqlDatabases:
         return False, f'{targetname} 不存在'
 
     def search_by_id(self, targetid):
+        # 根据学号搜索学生信息
+        # 遍历学生数据，找到学号匹配的学生并返回学生信息
+        # 如果找到匹配的学生，返回成功和学生信息
+        # 如果找不到匹配的学生，返回学生不存在的消息
         for stu in self.students:
             print(stu)
             if stu['id'] == targetid:
                 return True, stu
         return False, f'学号为 {targetid} 的学生不存在'
 
-    def update(self, targetstu):
-        for stu in self.students:
-            print(stu)
-            if stu['name'] == targetstu['name']:
-                stu.update(targetstu)
-                with open('students.json', mode='w', encoding='utf-8') as f:
-                    json.dump(self.students, f, ensure_ascii=False)
-                return True, f'{targetstu["name"]} 学生信息修改成功'
-        return False, f'{targetstu["name"]} 不存在'
-
+    def update(self, targetstu:dict):
+        # 更新学生信息
+        # 首先验证学生的邮箱格式是否正确
+        # 如果邮箱格式正确，遍历学生数据，找到姓名匹配的学生并更新信息
+        # 如果成功更新，将更新后的学生数据写入文件，并返回更新成功的消息
+        # 如果找不到匹配的学生，返回学生不存在的消息
+        # 如果邮箱格式不正确，返回插入失败的消息
+        if not self.is_valid_email(targetstu['email']):
+            return False, '邮箱格式不正确'
+        else:
+            for stu in self.students:
+                print(stu)
+                if stu['name'] == targetstu['name']:
+                    stu.update(targetstu)
+                    with open('students.json', mode='w', encoding='utf-8') as f:
+                        json.dump(self.students, f, ensure_ascii=False)
+                    return True, f'{targetstu["name"]} 学生信息修改成功'
+            return False, f'{targetstu["name"]} 不存在'
 
 db = MysqlDatabases()
+# 可以通过该实例调用数据库操作的方法
+
--- MenuViews.py
+++ MenuViews.py
@@ -5,8 +5,10 @@ from DataBase import db
 
 class AboutFrame(tk.Frame):
     def __init__(self, root):
+        # 初始化关于界面
         super().__init__(root)
-        tk.Label(self, text='By:add').pack()
+        # 关于信息
+        tk.Label(self, text='By:add(By:ymr)').pack()
         tk.Label(self, text='1395918522@qq.com').pack()
         tk.Label(self, text='https://github.com/add2333/StudentManager').pack()
 
@@ -14,6 +16,8 @@ class AboutFrame(tk.Frame):
 class ChangeFrame(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
+
+        # 创建用于存储用户输入的变量
         self.name = tk.StringVar()
         self.id = tk.StringVar()
         self.GPA = tk.StringVar()
@@ -21,13 +25,18 @@ class ChangeFrame(tk.Frame):
         self.status = tk.StringVar()
         self.sex = tk.StringVar()
         self.email = tk.StringVar()
+
+        # 构建页面
         self.build_page()
 
     def build_page(self):
+        # 构建页面，创建并排列标签、输入字段和按钮
+
+        # 创建一个空的标签作为占位符
         tk.Label(self).grid(row=0, pady=20)
 
+        # 创建标签和输入框用于输入学生信息
         tk.Label(self, text="仅支持通过姓名修改").grid(row=0, column=2, pady=10)
-
         tk.Label(self, text='姓  名:').grid(row=1, column=1, pady=10)
         tk.Entry(self, textvariable=self.name).grid(row=1, column=2, pady=10)
         tk.Label(self, text='学  号:').grid(row=2, column=1, pady=10)
@@ -41,13 +50,21 @@ class ChangeFrame(tk.Frame):
         tk.Label(self, text='班  级:').grid(row=6, column=1, pady=10)
         tk.Entry(self, textvariable=self.classes).grid(row=6, column=2, pady=10)
 
+        # 创建按钮，用于触发查询信息和修改信息的方法
         tk.Button(self, text="查询", command=self.search_info).grid(row=7, column=1, pady=10)
         tk.Button(self, text="修改", command=self.change_info).grid(row=7, column=2, pady=10)
+
+        # 创建标签，用于显示查询和修改操作的状态信息
         tk.Label(self, textvariable=self.status).grid(row=8, column=2, pady=10)
 
     def search_info(self):
+        # 查询信息的方法
+
+        # 调用数据库的方法，通过姓名查询学生信息
         isexist, info = db.search_by_name(self.name.get())
+
         if isexist:
+            # 如果查询结果存在，则将查询到的学生信息显示在对应的输入框中
             self.name.set(info['name'])
             self.id.set(info['id'])
             self.GPA.set(info['GPA'])
@@ -56,22 +73,39 @@ class ChangeFrame(tk.Frame):
             self.sex.set(info['sex'])
             self.status.set('数据查询成功')
         else:
+            # 如果查询结果不存在，则将状态信息设置为查询失败的原因
             self.status.set(info)
 
         pass
 
     def change_info(self):
-        stu = {"name": self.name.get(), "id": self.id.get(), "GPA": self.GPA.get(), "class": self.classes.get()}
+        # 修改信息的方法
+
+        # 创建一个字典，存储要修改的学生信息
+        stu = {"name": self.name.get(), "id": self.id.get(), "GPA": self.GPA.get(),
+               "class": self.classes.get(), "email": self.email.get(), "sex": self.sex.get()}
+
+        # 清空输入框中的内容
         self.name.set('')
         self.id.set('')
         self.GPA.set('')
         self.classes.set('')
         self.sex.set('')
         self.email.set('')
-        # print(stu)
-        db.update(stu)
-        self.status.set('修改{%s}同学的信息成功！' % (stu["name"]))
+
+        # 调用数据库的方法，更新学生信息
+        isupdated, message = db.update(stu)
+
+        if isupdated:
+            # 设置状态信息为成功修改学生信息的消息
+            self.status.set('修改{%s}同学的信息成功！' % (stu["name"]))
+        else:
+            # 如果更新失败，则弹出警告框显示更新失败的原因，并设置状态信息为更新失败的消息
+            messagebox.showwarning(title='更新失败', message=message)
+            self.status.set(message)
+
         pass
+
 
 
 class InsertFrame(tk.Frame):
@@ -87,8 +121,8 @@ class InsertFrame(tk.Frame):
         self.build_page()
 
     def build_page(self):
+        # 构建页面，创建并排列标签、输入字段和按钮
         tk.Label(self).grid(row=0, pady=20)
-
         tk.Label(self, text='姓  名:').grid(row=1, column=1, pady=10)
         tk.Entry(self, textvariable=self.name).grid(row=1, column=2, pady=10)
         tk.Label(self, text='学  号:').grid(row=2, column=1, pady=10)
@@ -106,6 +140,7 @@ class InsertFrame(tk.Frame):
         tk.Label(self, textvariable=self.status).grid(row=8, column=2, pady=10)
 
     def record_info(self):
+        # 记录信息
         stu = {"name": self.name.get(), "id": self.id.get(), "GPA": self.GPA.get(), "class": self.classes.get(),
                "email": self.email.get(), "sex": self.sex.get()}
         self.name.set('')
@@ -126,16 +161,23 @@ class InsertFrame(tk.Frame):
 
 class SearchFrame(tk.Frame):
     def __init__(self, root):
+        # 初始化界面
         super().__init__(root)
-
         self.table_view = tk.Frame()
         self.table_view.pack()
 
         self.build_page()
 
     def build_page(self):
+        # 构建页面，创建并排列表格和刷新按钮
+
+        # 定义表格的列名
         columns = ("name", "id", "GPA", "class", "sex", "email")
+
+        # 创建一个Treeview控件用于显示表格
         self.tree_view = ttk.Treeview(self, show='headings', columns=columns)
+
+        # 设置每列的宽度和对齐方式
         self.tree_view.column('name', width=100, anchor='center')
         self.tree_view.column('id', width=200, anchor='center')
         self.tree_view.column('GPA', width=100, anchor='center')
@@ -143,32 +185,38 @@ class SearchFrame(tk.Frame):
         self.tree_view.column('sex', width=100, anchor='center')
         self.tree_view.column('email', width=300, anchor='center')
 
+        # 设置每列的标题
         self.tree_view.heading('name', text='姓名')
         self.tree_view.heading('id', text='学号')
         self.tree_view.heading('GPA', text='学分绩')
         self.tree_view.heading('class', text='班级')
         self.tree_view.heading('sex', text='性别')
         self.tree_view.heading('email', text='邮箱')
-        self.tree_view.pack(fill=tk.BOTH, expand=True)
 
-        # 设置行高
+        # 设置表格行的高度
         style = ttk.Style()
         style.configure("Treeview", rowheight=50)
         self.tree_view["style"] = "Treeview"
 
+        # 将表格填充到父容器中
         self.tree_view.pack(fill=tk.BOTH, expand=True)
+
+        # 显示数据库中的学生信息
         self.display_data_frame()
 
+        # 创建刷新按钮，并绑定刷新数据的方法
         tk.Button(self, text='刷新', command=self.display_data_frame).pack()
 
     def display_data_frame(self):
-        # 删除旧的信息
+        # 清空表格中的旧数据
         for _ in map(self.tree_view.delete, self.tree_view.get_children('')):
             pass
 
+        # 从数据库中获取所有学生的信息
         students = db.all_student()
+
+        # 将学生信息逐行插入表格中
         for index, stu in enumerate(students):
-            # print(stu)
             self.tree_view.insert('', index, values=(stu['name'], stu['id'], stu['GPA'],
                                                      stu['class'], stu['sex'], stu['email']))
 
@@ -176,6 +224,8 @@ class SearchFrame(tk.Frame):
 class SearchByIdFrame(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
+
+        # 创建用于存储用户输入的变量
         self.name = tk.StringVar()
         self.id = tk.StringVar()
         self.GPA = tk.StringVar()
@@ -183,11 +233,16 @@ class SearchByIdFrame(tk.Frame):
         self.status = tk.StringVar()
         self.sex = tk.StringVar()
         self.email = tk.StringVar()
+
         self.build_page()
 
     def build_page(self):
+        # 构建页面，包括标签、输入框和按钮
+
+        # 创建一个空的标签作为占位符
         tk.Label(self).grid(row=0, pady=10)
 
+        # 创建标签和输入框用于输入学生信息
         tk.Label(self, text='姓  名:').grid(row=1, column=1, pady=10)
         tk.Entry(self, textvariable=self.name).grid(row=1, column=2, pady=10)
         tk.Label(self, text='学  号:').grid(row=2, column=1, pady=10)
@@ -201,12 +256,20 @@ class SearchByIdFrame(tk.Frame):
         tk.Label(self, text='班  级:').grid(row=6, column=1, pady=10)
         tk.Entry(self, textvariable=self.classes).grid(row=6, column=2, pady=10)
 
+        # 创建按钮，用于触发通过学号查询的方法
         tk.Button(self, text="通过学号查询", command=self.search_info_by_id).grid(row=7, column=2, pady=10)
+
+        # 创建标签，用于显示查询结果的状态信息
         tk.Label(self, textvariable=self.status).grid(row=8, column=2, pady=10)
 
     def search_info_by_id(self):
+        # 通过学号查询学生信息的方法
+
+        # 调用数据库的方法，通过学号查询学生信息
         isexist, info = db.search_by_id(self.id.get())
+
         if isexist:
+            # 如果查询结果存在，则将查询到的学生信息显示在对应的输入框中
             self.name.set(info['name'])
             self.id.set(info['id'])
             self.GPA.set(info['GPA'])
@@ -215,6 +278,7 @@ class SearchByIdFrame(tk.Frame):
             self.email.set(info['email'])
             self.status.set('数据查询成功')
         else:
+            # 如果查询结果不存在，则显示查询失败的原因
             self.status.set(info)
 
         pass
@@ -223,7 +287,8 @@ class SearchByIdFrame(tk.Frame):
 class SearchByNameFrame(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
-        super().__init__(root)
+
+        # 创建用于存储用户输入的变量
         self.name = tk.StringVar()
         self.id = tk.StringVar()
         self.GPA = tk.StringVar()
@@ -231,11 +296,16 @@ class SearchByNameFrame(tk.Frame):
         self.status = tk.StringVar()
         self.sex = tk.StringVar()
         self.email = tk.StringVar()
+
         self.build_page()
 
     def build_page(self):
+        # 构建页面，包括标签、输入框和按钮
+
+        # 创建一个空的标签作为占位符
         tk.Label(self).grid(row=0, pady=10)
 
+        # 创建标签和输入框用于输入学生信息
         tk.Label(self, text='姓  名:').grid(row=1, column=1, pady=10)
         tk.Entry(self, textvariable=self.name).grid(row=1, column=2, pady=10)
         tk.Label(self, text='学  号:').grid(row=2, column=1, pady=10)
@@ -249,12 +319,20 @@ class SearchByNameFrame(tk.Frame):
         tk.Label(self, text='班  级:').grid(row=6, column=1, pady=10)
         tk.Entry(self, textvariable=self.classes).grid(row=6, column=2, pady=10)
 
+        # 创建按钮，用于触发通过姓名查询的方法
         tk.Button(self, text="通过姓名查询", command=self.search_info_by_name).grid(row=7, column=2, pady=10)
+
+        # 创建标签，用于显示查询结果的状态信息
         tk.Label(self, textvariable=self.status).grid(row=8, column=2, pady=10)
 
     def search_info_by_name(self):
+        # 通过姓名查询学生信息的方法
+
+        # 调用数据库的方法，通过姓名查询学生信息
         isexist, info = db.search_by_name(self.name.get())
+
         if isexist:
+            # 如果查询结果存在，则将查询到的学生信息显示在对应的输入框中
             self.name.set(info['name'])
             self.id.set(info['id'])
             self.GPA.set(info['GPA'])
@@ -263,6 +341,7 @@ class SearchByNameFrame(tk.Frame):
             self.email.set(info['email'])
             self.status.set('数据查询成功')
         else:
+            # 如果查询结果不存在，则将状态信息设置为查询失败的原因
             self.status.set(info)
 
         pass
@@ -271,18 +350,31 @@ class SearchByNameFrame(tk.Frame):
 class DeleteFrame(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
+
+        # 创建用于存储用户输入的变量
         self.username = tk.StringVar()
         self.status = tk.StringVar()
+
+        # 创建标签、输入框和按钮
         tk.Label(self, text='根据名称删除数据').pack()
         tk.Entry(self, textvariable=self.username).pack()
         tk.Button(self, text='删除', command=self.delete).pack()
         tk.Label(self, textvariable=self.status).pack()
 
     def delete(self):
+        # 删除数据的方法
+
+        # 获取用户输入的用户名
         username = self.username.get()
+
+        # 调用数据库的方法，根据名称删除数据
         isdelete, message = db.delete_by_name(username)
+
         if isdelete:
+            # 如果删除成功，则设置状态信息为删除成功的消息
             self.status.set(message)
         else:
+            # 如果删除失败，则弹出警告框显示删除失败的原因
             messagebox.showwarning(title='删除失败', message=message)
+
         pass
